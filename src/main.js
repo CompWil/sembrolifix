fa8bbaa0d41a71b082d27dcfa94738dbfa9d4f33
Wilifri <wiliafri12@gmail.com>
2026-02-24 10:25:02 +0700

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-
+import Chart from 'chart.js/auto';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
